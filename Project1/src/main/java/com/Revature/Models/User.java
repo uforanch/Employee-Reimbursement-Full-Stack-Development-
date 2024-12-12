cf39d22f1d204cea35c90cdf1d95986ae5806348
Nathaniel Ince <52634828+uforanch@users.noreply.github.com>
2024-12-11 19:18:52 -0500

    First Commit

---
+++ Project1/src/main/java/com/Revature/Models/User.java
@@ -0,0 +1,61 @@
+package com.Revature.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String role = "Employee";
+
+
+    public User() {
+    }
+
+    public User(int userId, String name, String role) {
+        this.userId = userId;
+        this.name = name;
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
